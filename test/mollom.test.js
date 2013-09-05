4037c9458dce332d86eb8f5e48c256ff7e78fa0e
Laurent Chardin <laurent@chardin.org>
2013-09-05 09:53:28 +0200

    Adding files

---
+++ test/mollom.test.js
@@ -0,0 +1,151 @@
+#!/usr/bin/env node
+
+var mollom = require('../index')
+  , async = require('async')
+  ;
+
+var mollomclient = new mollom({
+    consumer_key: null
+  , consumer_secret: null
+});
+
+mollomclient._check(function(a) {
+  console.log(a);
+});
+
+/**
+ * POST http://rest.mollom.com/v1/site
+ */
+
+var newsite = {
+  url: 'localhost',
+  email: 'laurent@chardin.org'
+};
+
+mollomclient.createSite(newsite, function(err, res, body) {
+  console.log(err);
+  console.log(res);
+  console.log(body);
+
+  if (res.statusCode == '200') {
+    console.log('Site created !!');
+    site = body.site;
+  }
+});
+
+/*
+
+var url = 'http://dev.mollom.com/v1/site',
+    form = {
+      url: 'localhost',
+      email: 'laurent@chardin.org',
+      platformName: 'nodejs',
+      platformVersion: '0.10.16',
+      clientName: 'node-mollom',
+      clientVersion: '0.0.1'
+    };
+
+
+req.post({url: url, form: form, json: true}, function (e, r, body) {
+
+  console.log(r.statusCode);
+  console.log(body);
+
+  if (r.statusCode == '200') {
+    console.log('Site created !!');
+    site = body.site;
+
+
+    //GET http://dev.mollom.com/v1/site
+    url = 'http://dev.mollom.com/v1/site/' + site.publicKey;
+
+    req.get({url: url, json: true}, function (e, r, body) {
+      console.log(r.statusCode);
+      console.log(body);
+
+      if (r.statusCode == '200') {
+        console.log('Site requested !!');
+
+        // POST http://dev.mollom.com/v1/site/{publicKey}/delete
+        url = 'http://dev.mollom.com/v1/site/' + site.publicKey + '/delete';
+        oauth =
+        { consumer_key: site.publicKey,
+          consumer_secret: site.privateKey
+        };
+        req.post({url: url, oauth: oauth, json: true}, function (e, r, body) {
+          console.log(r.statusCode);
+          console.log(body);
+
+          if (r.statusCode == '200') {
+            console.log('Site deleted !!');
+          }
+        });
+      }
+    });
+  }
+});
+
+*/
+
+/**
+ * POST http://dev.mollom.com/v1/content
+ */
+/*
+
+var url = 'http://dev.mollom.com/v1/content',
+    oauth =
+    { consumer_key: "ifnbhz6na17h12dyzohglcj6d83wokoq",
+      consumer_secret: "1s6wrpgzl6ydvtdxqkn9yrz82eubktjp"
+    },
+    form = {
+      postTitle: 'ham', // for testing: spam, ham and unsure
+      postBody: '',
+      checks: 'spam', //{"spam"|"quality"|"profanity"|"language"}
+      unsure: '1',
+      //strictness: 'normal', //{"strict"|"normal"|"relaxed"}
+      //authorName: 'authorName',
+      //authorEmail: 'laurent@chardin.org',
+      //authorIp: '127.0.0.1',
+      //authorId: 'authorId',
+      //contextUrl: '',
+      contextTitle: 'Testing mode'
+    };
+
+req.post({url: url, oauth: oauth, form: form, json: true}, function (e, r, body) {
+
+  if(r.statusCode == 200) {
+    console.log('Ok');
+  }
+  else {
+    console.log('Error');
+  }
+
+  console.log(body);
+});
+
+*/
+
+var data = {
+  postTitle: 'ham', // for testing: spam, ham and unsure
+  postBody: '',
+  checks: 'spam', //{"spam"|"quality"|"profanity"|"language"}
+  unsure: '1',
+  //strictness: 'normal', //{"strict"|"normal"|"relaxed"}
+  //authorName: 'authorName',
+  //authorEmail: 'laurent@chardin.org',
+  //authorIp: '127.0.0.1',
+  //authorId: 'authorId',
+  //authorOpenid:
+  //rateLimit
+  //honeypot
+  //stored
+  //url
+  //contextUrl: '',
+  contextTitle: 'Testing mode'
+};
+
+mollomclient.contentCheck(data, function(err, res, body) {
+  console.log(err);
+  console.log(res);
+  console.log(body);
+});
